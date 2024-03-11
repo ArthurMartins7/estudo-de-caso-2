a53f2cfc33f514868fc0a8ac56e7928023447b23
ArthurMartins7 <showzin048@gmail.com>
2024-03-11 09:59:14 -0300

    commit para usar no pc do senac  pra fazer os metodos crud de pessoa

---
+++ src/main/java/model/entity/Aplicacao.java
@@ -0,0 +1,13 @@
+package model.entity;
+
+import java.time.LocalDate;
+
+public class Aplicacao {
+	
+	private LocalDate dataDaAplicacao;
+	private Pessoa pessoaAplicada;
+	private Vacina vacinaAplicada;
+	
+	
+
+}
--- src/main/java/model/entity/Vacina.java
+++ src/main/java/model/entity/Vacina.java
@@ -3,9 +3,47 @@ package model.entity;
 import java.time.LocalDate;
 
 public class Vacina {
-	
+
 	private String paisDeOrigem;
 	private int estagioDaPesquisa;
 	private LocalDate dataDeInicioDaPesquisa;
 	private Pessoa pesquisador;
+
+	public Vacina() {
+		super();
+
+	}
+
+	public String getPaisDeOrigem() {
+		return paisDeOrigem;
+	}
+
+	public void setPaisDeOrigem(String paisDeOrigem) {
+		this.paisDeOrigem = paisDeOrigem;
+	}
+
+	public int getEstagioDaPesquisa() {
+		return estagioDaPesquisa;
+	}
+
+	public void setEstagioDaPesquisa(int estagioDaPesquisa) {
+		this.estagioDaPesquisa = estagioDaPesquisa;
+	}
+
+	public LocalDate getDataDeInicioDaPesquisa() {
+		return dataDeInicioDaPesquisa;
+	}
+
+	public void setDataDeInicioDaPesquisa(LocalDate dataDeInicioDaPesquisa) {
+		this.dataDeInicioDaPesquisa = dataDeInicioDaPesquisa;
+	}
+
+	public Pessoa getPesquisador() {
+		return pesquisador;
+	}
+
+	public void setPesquisador(Pessoa pesquisador) {
+		this.pesquisador = pesquisador;
+	}
+
 }
