612dade658e4cb4af0ebe60ad0873d11531bbd39
ArthurMartins7 <showzin048@gmail.com>
2024-03-11 09:37:09 -0300

    primeito commit para o pc do senac

---
+++ src/main/java/model/entity/Pessoa.java
@@ -0,0 +1,49 @@
+package model.entity;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+
+	private String nome;
+	private LocalDate dataNascimento;
+	private char sexo;
+	private long cpf;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Pessoa() {
+		super();
+
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+
+}
---
+++ src/main/java/model/entity/Vacina.java
@@ -0,0 +1,11 @@
+package model.entity;
+
+import java.time.LocalDate;
+
+public class Vacina {
+	
+	private String paisDeOrigem;
+	private int estagioDaPesquisa;
+	private LocalDate dataDeInicioDaPesquisa;
+	private Pessoa pesquisador;
+}
